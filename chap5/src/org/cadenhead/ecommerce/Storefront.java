0c056a3c53e39536fd14b083d9d5cfb60607730c
xinli_xiao <xiaoxinlixxl@gmail.com>
2015-01-12 22:57:03 -0600

    try to learn implements from chap6 - more

--- chap5/src/org/cadenhead/ecommerce/Storefront.java
+++ chap5/src/org/cadenhead/ecommerce/Storefront.java
@@ -1,7 +1,30 @@
 package org.cadenhead.ecommerce;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 /**
  * Created by xiao on 1/7/2015.
  */
 public class Storefront {
+
+    private LinkedList catalog =new LinkedList();
+
+    public void addItem(String id, String name, String price, String quant){
+        Item it =new Item(id, name, price ,quant);
+        catalog.add(it);
+
+    }
+
+    public Item getItem(int i){
+        return (Item)catalog.get(i);
+    }
+
+    public int getSize(){
+        return catalog.size();
+    }
+
+    public void sort(){
+        Collections.sort(catalog);
+    }
 }
